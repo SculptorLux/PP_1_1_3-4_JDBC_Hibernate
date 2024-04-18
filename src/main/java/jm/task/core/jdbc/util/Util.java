d2138a5564f7c7e37ed7dcd41c2abbc994998817
SculptorLux <alexanisin@gmail.com>
2024-04-18 15:42:32 +0300

    Подключение к базе данных

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,31 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/db_users";
+
+    private final static String USERNAME = "root";
+
+    private final static String PASSWORD = "Super268413975!";
+
+
+    public static void main(String[] args) {
+
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+
+            if (!connection.isClosed()) {
+
+                System.out.println("We are connected!");
+
+            }
+
+        } catch (SQLException e) {
+
+            System.out.println("there is no connection... Exception!");
+
+        }
+    }
 }
