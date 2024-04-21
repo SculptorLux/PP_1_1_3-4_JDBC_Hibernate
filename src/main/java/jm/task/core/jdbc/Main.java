ecc79a341f3c4648fa92297b10dcef858474d97c
SculptorLux <alexanisin@gmail.com>
2024-04-22 02:09:12 +0300

    Убрал лишний коннект к базе данных

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Util.getConnection();
         UserDao userDao = new UserDaoJDBCImpl(Util.getConnection());
 
         userDao.createUsersTable();
