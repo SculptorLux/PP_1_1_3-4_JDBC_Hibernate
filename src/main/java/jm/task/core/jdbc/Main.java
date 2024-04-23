83e9ddc8fbf528809e080799b52d497d857a7b70
SculptorLux <alexanisin@gmail.com>
2024-04-23 16:41:30 +0300

    Подключение Hibernate и настройка зависимостей Осталось реализовать методы для работы с бд

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,22 +1,21 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.util.Util;
-
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import java.sql.SQLException;
+
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        UserDao userDao = new UserDaoJDBCImpl(Util.getConnection());
+        UserService userService = new UserServiceImpl();
 
-        userDao.createUsersTable();
-        userDao.saveUser("Sasha1", "SashaLastName1", (byte) 23);
-        userDao.saveUser("Lolia2", "KoliaLastName2", (byte) 22);
-        userDao.saveUser("Nikita3", "NikitaLastName3", (byte) 31);
-        userDao.saveUser("Bob4", "BobLastName4", (byte) 30);
-        userDao.getAllUsers();
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+        userService.createUsersTable();
+        userService.saveUser("Sasha1", "SashaLastName1", (byte) 23);
+        userService.saveUser("Lolia2", "KoliaLastName2", (byte) 22);
+        userService.saveUser("Nikita3", "NikitaLastName3", (byte) 31);
+        userService.saveUser("Bob4", "BobLastName4", (byte) 30);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class Util {
     private final static String URL = "jdbc:mysql://localhost:3306/db_users";
@@ -23,4 +31,44 @@ public class Util {
         }
         return connection;
     }
+
+    public static class HbFactoryConfiguration {
+        private static HbFactoryConfiguration hbFactoryConfiguration;
+        private SessionFactory sessionFactory;
+        //create session factory
+
+        private HbFactoryConfiguration() {
+            //add configuration
+            Configuration configuration = new Configuration();
+
+            //add property
+            Properties properties = new Properties();
+
+            //add already created hibernate file to properties in current thread
+            try {
+                properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("src.main.resources.hibernate.properties"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //add properties to configure
+            configuration.setProperties(properties);
+
+            //add annotated class configure
+            configuration.addAnnotatedClass(User.class);
+
+            //build session factory
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        public static HbFactoryConfiguration getInstance() {
+            return (hbFactoryConfiguration == null) ? new HbFactoryConfiguration() : hbFactoryConfiguration;
+        }
+        public Session getSession() {
+            return sessionFactory.openSession();
+            //open session and return it
+        }
+        //this class implemented singleton pattern
+
+    }
+
 }
